67db347798e5d9a5810f1fdf9f377f1809229b54
loptom <tomasrlopes@gmail.com>
2025-08-04 19:45:29 +0100

    Fix compilation error

--- x.c
+++ x.c
@@ -2179,7 +2179,7 @@ xrdb_load(void)
 		XRESOURCE_LOAD_INTEGER("blinktimeout", blinktimeout);
 		XRESOURCE_LOAD_INTEGER("bellvolume", bellvolume);
 		XRESOURCE_LOAD_INTEGER("borderpx", borderpx);
-		XRESOURCE_LOAD_INTEGER("cursorshape", cursorshape);
+		XRESOURCE_LOAD_INTEGER("cursorshape", cursorstyle);
 
 		XRESOURCE_LOAD_FLOAT("cwscale", cwscale);
 		XRESOURCE_LOAD_FLOAT("chscale", chscale);
